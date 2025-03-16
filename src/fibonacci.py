e0f44ccb477464b01e273dd0e2ea2235099118c5
Julio Tavares <juliotavares197@gmail.com>
2025-03-16 10:40:17 -0300

    chore: use settings varibles

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
+from settings import FIBONACCI_CACHE_SIZE
 
-@lru_cache(maxsize=100)
+@lru_cache(maxsize=FIBONACCI_CACHE_SIZE)
 def calculate_fibonacci(n: int) -> int:
     if n < 2:
         return n
